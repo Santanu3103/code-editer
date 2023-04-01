e656a97403313eb1ad3cacfee90b9d7969949fa6
santunu <santanuroy253396@gmail.com>
2023-04-02 00:24:04 +0530

    third commit

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -8,10 +8,10 @@ function Home() {
   const { isOpenModal } = useContext(ModalContext);
   return (
     <div className="grid grid-cols-1 lg:grid-cols-2">
-      <div className="sm:w-full md:w-full w-5/12">
+      <div className="sm:w-full md:w-full w-4/12">
         <LeftPaneHomeScreen />
       </div>
-      <div className="sm:w-full md:w-full w-7/12">
+      <div className="sm:w-full md:w-full w-8/12">
         <RightPaneHomeScreen />
       </div>
       {isOpenModal.show && <Modal />}
